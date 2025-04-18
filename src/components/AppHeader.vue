b1ce49b197dd29a0eab499a4ce2a5dd53b5fc3d3
TvoyKot <a.lshe@yandex.by>
2025-04-18 12:59:47 +0300

    Added menu and routing

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,30 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import type { ComputedRef } from 'vue'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
 interface IMenuItem {
   label: string
   icon: string
   path: string
+  show: ComputedRef<boolean>
 }
 const items = ref<IMenuItem[]>([
   {
     label: 'Авторизация',
     icon: 'pi pi-user',
     path: '/auth',
+    show: computed((): boolean => !userStore.userId),
   },
   {
     label: 'Добавить',
     icon: 'pi pi-plus',
     path: '/',
+    show: computed((): boolean => !!userStore.userId),
   },
   {
     label: 'Список собеседований',
     icon: 'pi pi-list',
     path: '/list',
+    show: computed((): boolean => !!userStore.userId),
   },
   {
     label: 'Статистика',
     icon: 'pi pi-chart-pie',
     path: '/statistic',
+    show: computed((): boolean => !!userStore.userId),
   },
 ])
 </script>
@@ -32,17 +41,27 @@ const items = ref<IMenuItem[]>([
 <template>
   <app-menubar :model="items" class="menu">
     <template #item="{ item, props }">
-      <router-link
-        :to="item.path"
-        v-bind="props.action"
-        class="flex align-items-center"
-      >
+      <template v-if="item.show">
+        <router-link
+          :to="item.path"
+          v-bind="props.action"
+          class="flex align-items-center"
+        >
+          <span
+            :class="item.icon"
+            class="p-menuitem-icon"
+          ></span>
+          <span class="ml-2"> {{ item.label }}</span>
+        </router-link>
+      </template>
+    </template>
+    <template #end>
+      <span v-if="userStore.userId" @click="userStore.userId = ''" class="flex align-items-center menu-exit">
         <span
-          :class="item.icon"
-          class="p-menuitem-icon"
+          class="pi pi-sign-out p-p-menuitem-icon"
         ></span>
-        <span class="ml-2"> {{ item.label }}</span>
-      </router-link>
+        <span>Выход</span>
+      </span>
     </template>
   </app-menubar>
 </template>
